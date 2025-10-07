aa3e3725fcca14bd79741165f8cc2baa6d7c7ed5
Estefa7 <kolibri13742@gmail.com>
2025-10-07 23:09:33 +0200

    adding more colors, background, more items

--- src/data/shoppingList.js
+++ src/data/shoppingList.js
@@ -1,9 +1,16 @@
 export const shoppingList = {
     id: "1",
     name: "Party snacks",
+    owner: "Anna",
     members: ["You", "Bell"],
     items: [
         { id: 1, name: "Chips", resolved: true},
         {id:2, name: "Sprite", resolved: false},
+        {id:3, name: "Beer ", resolved: false},
+        {id:4, name: "Nachos", resolved: true},
+        {id:5, name: "Pizza", resolved: false},
+        {id:6, name: "Salsa", resolved: true},
+        {id:7, name: "Baby carrots", resolved: false},
+        {id:8, name: "Hummus", resolved: false},
     ],
 };
